74f2b0b32aafa3b828b9b59fe8fd149272fd343b
faizerumar <faizerumar@gmail.com>
2026-09-23 01:02:36 +0530

    Updated portfolio styling and content

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: '/1Portfolio Umar/',
+    base: '/1Portfolio-Umar/',
     plugins: [react()],
     build: {
         rollupOptions: {
